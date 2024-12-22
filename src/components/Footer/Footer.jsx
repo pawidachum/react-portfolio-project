7f2d9a4a0bdc7ea808db077b7605d9e9f97c99c3
pawida <pawida.may@gmail.com>
2024-12-22 23:17:02 +0700

    Apply CSS module styling to footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 
 function Footer() {
   return (
-    <footer>
+    <footer className={styles.footer}>
         <p>Copyright © 2023. All Rights Reserved.</p>
     </footer>
   )
